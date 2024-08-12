da471e7796a254a9bd4ce2e3407e28437215e929
Maksym Motnyk <maksummotnuk@gmail.com>
2024-08-12 22:10:24 +0300

    Homework-7

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+import { fetchContacts } from "../../redux/contactsOps";
+import {
+  selectFilteredContacts,
+  selectLoading,
+  selectError,
+} from "../../redux/contactsSlice";
+
+export default function ContactList() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectFilteredContacts);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading contacts...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading contacts: {error}</p>;
+  }
+
+  return (
+    <div>
+      <ul className={css.listItem}>
+        {contacts.map((contact) => (
+          <li key={contact.id} className={css.contactItem}>
+            <Contact contact={contact} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
